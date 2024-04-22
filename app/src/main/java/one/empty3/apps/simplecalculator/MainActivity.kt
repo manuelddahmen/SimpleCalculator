015c70ff61408192bc77ee18d7507c55df78eed2
Manuel Daniel Dahmen <manuel.dahmen@gmx.com>
2024-04-22 09:48:10 +0200

    Update Calculator to latest version + Power sign ^ bug : 2^ <-- slow Sync library1 package with empty3-library-3d Minus sign starting expression: SOLVED Try to fix : non starting app on Android 10

--- app/src/main/java/one/empty3/apps/simplecalculator/MainActivity.kt
+++ app/src/main/java/one/empty3/apps/simplecalculator/MainActivity.kt
@@ -20,15 +20,18 @@
 
 package one.empty3.apps.simplecalculator
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.set
 import androidx.preference.PreferenceManager
 import one.empty3.library.StructureMatrix
 import one.empty3.library1.tree.AlgebraicFormulaSyntaxException
@@ -68,9 +71,12 @@ class MainActivity : AppCompatActivity() {
         val textAnswer: EditText = findViewById<EditText>(R.id.answerText)
         val editText = findViewById<EditText>(R.id.editTextCalculus)
         var string: String? = prefs.getString("autoSaveEditText", "")
-        if(string!=null)
+        if (string != null)
             editText.setText(string)
-
+        if(string!=null) {
+            val tree = AlgebraicTree(string)
+            compute(tree, textAnswer)
+        }
         for (j: Int in buttonsNumbers) {
             val findViewById: Button = findViewById(j)
             findViewById.setOnClickListener {
@@ -97,7 +103,6 @@ class MainActivity : AppCompatActivity() {
 //                val ft: FragmentTransaction = supportFragmentManager.beginTransaction();
 //                ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
             val dialog = ChooseFunctionDialogFragment()
-            val stringArrayAdapter = StringArrayAdapter()
             dialog.show(
                 supportFragmentManager,
                 "one.empty3.apps.simplecalculator.ChooseFunctionDialogFragment"
@@ -115,33 +120,40 @@ class MainActivity : AppCompatActivity() {
             }.start()
         }
         // Create an instance of the dialog fragment and show it
-            //val dialog = ChooseFunctionDialogFragment()
-            //dialog.show(
-            //    getSupportFragmentManager(),
-            //    "one.empty3.apps.simplecalculator.ChooseFunctionDialogFragment"
-            //)
+        //val dialog = ChooseFunctionDialogFragment()
+        //dialog.show(
+        //    getSupportFragmentManager(),
+        //    "one.empty3.apps.simplecalculator.ChooseFunctionDialogFragment"
+        //)
         //       this.setContentView(R.layout.fragment_item_list)
 //            editText.text = editText.text.append(dialog.function)
         findViewById<Button>(R.id.AboutButton).setOnClickListener {
             openUserData(it)
         }
         findViewById<Button>(R.id.textCalculatorButton).setOnClickListener(View.OnClickListener {
-            val intentText:Intent = Intent(Intent.ACTION_EDIT)
+            val intentText: Intent = Intent(Intent.ACTION_EDIT)
             intentText.setClass(applicationContext, ScrollingActivity::class.java)
             startActivity(intentText)
         })
 
         editText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                val tree = AlgebraicTree(editText.text.toString())
+                val tree = AlgebraicTree(s.toString())
                 compute(tree, textAnswer)
             }
-            override fun beforeTextChanged(s:CharSequence, start:Int, count:Int, after:Int) {
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
 
             }
+
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                var toString = editText?.text?.toString()
-                if(toString==null)
+                var toStringSeq = s
+                var toString = ""
+                if(s==null)
+                    toString = ""
+                else
+                    toString = s.toString()
+                if (toString == null)
                     toString = ""
                 prefs.edit().putString("autoSaveEditText", toString).apply();
             }
@@ -151,36 +163,52 @@ class MainActivity : AppCompatActivity() {
 
     private fun compute(
         tree: AlgebraicTree,
-        textAnswer: EditText) {
+        textAnswer: EditText
+    ) {
+        val updateAnswer = Runnable {
             try {
-            tree.construct()
-            val d: StructureMatrix<Double>? = tree.eval()
-            textAnswer.text.append(stringFromEval(d))
+                if (tree == null) {
+                    textAnswer.setText("")
+                    return@Runnable
+                }
+                tree.construct()
+                val d: StructureMatrix<Double>? = tree.eval()
+                val str = stringFromEval(d)
+                if (str == null || str.equals("")) {
+                    textAnswer.setText("")
+                    return@Runnable
+                }
+                textAnswer.setText(str)
                 textAnswer.setTextColor(Color.BLACK)
-        } catch (ex: AlgebraicFormulaSyntaxException) {
-            textAnswer.setTextColor(Color.RED)
-            textAnswer.text.append("")
-        } catch (ex: IndexOutOfBoundsException) {
-            textAnswer.setTextColor(Color.RED)
-            textAnswer.text.append("")
-        } catch (ex: NullPointerException) {
-            textAnswer.setTextColor(Color.RED)
-            textAnswer.text.append("")
+            } catch (ex: AlgebraicFormulaSyntaxException) {
+                textAnswer.setTextColor(Color.RED)
+                textAnswer.setText("")
+            } catch (ex: IndexOutOfBoundsException) {
+                textAnswer.setTextColor(Color.RED)
+                textAnswer.setText("")
+            } catch (ex: NullPointerException) {
+                textAnswer.setTextColor(Color.RED)
+                textAnswer.setText("")
+            }
         }
+        val mainHandler :Handler= Handler(applicationContext.getMainLooper())
+        mainHandler.post(updateAnswer)
     }
 
     private fun stringFromEval(d: StructureMatrix<Double>?): String {
-        var labelAnswer: String = ""+ (d?.getElem() ?: 0.0)
-        if(d!=null && d.dim==0)
-            labelAnswer =""+ d.getElem()
-        else if(d!=null &&d.dim==1 && d.data1d!=null) {
+        if (d == null)
+            return ""
+        var labelAnswer: String = "" + (d.getElem() ?: 0.0)
+        if (d.dim == 0)
+            labelAnswer = "" + d.getElem()
+        else if (d.dim == 1 && d.data1d != null) {
             labelAnswer = "("
             for (i in 0 until d.data1d.size) {
-                labelAnswer+=d.data1d[i]
-                if(i<d.data1d.size-1)
-                    labelAnswer+=","
+                labelAnswer += d.data1d[i]
+                if (i < d.data1d.size - 1)
+                    labelAnswer += ","
             }
-            labelAnswer+=")"
+            labelAnswer += ")"
         }
         return labelAnswer
     }
