aec9bd745f7a49fcd821c3588ccb19156f7c7dc6
Manuel Daniel Dahmen <manuel.dahmen@gmx.com>
2024-05-02 11:54:34 +0200

    Error on compute c=a+b First try to repair See assignation

--- app/src/main/java/one/empty3/library1/tree/ListInstructions.java
+++ app/src/main/java/one/empty3/library1/tree/ListInstructions.java
@@ -1,21 +1,19 @@
 /*
+ * Copyright (c) 2023.
  *
- *  * Copyright (c) 2024. Manuel Daniel Dahmen
- *  *
- *  *
- *  *    Copyright 2024 Manuel Daniel Dahmen
- *  *
- *  *    Licensed under the Apache License, Version 2.0 (the "License");
- *  *    you may not use this file except in compliance with the License.
- *  *    You may obtain a copy of the License at
- *  *
- *  *        http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  *    Unless required by applicable law or agreed to in writing, software
- *  *    distributed under the License is distributed on an "AS IS" BASIS,
- *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  *    See the License for the specific language governing permissions and
- *  *    limitations under the License.
+ *
+ *  Copyright 2012-2023 Manuel Daniel Dahmen
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  *
  */
@@ -32,15 +30,10 @@ import java.util.Locale;
 import one.empty3.library.StructureMatrix;
 
 public class ListInstructions {
-    private HashMap<String, Double> currentParamsValues = new HashMap<>();
-    private HashMap<String, String> currentParamsValuesVec = new HashMap<>();
-    private HashMap<String, StructureMatrix<Double>> currentParamsValuesVecComputed = new HashMap<>();
-
-    public String evaluate(String s) {
-        return "";
-    }
-
-    public static class Instruction {
+    HashMap<String, Double> currentParamsValues = new HashMap<>();
+    HashMap<String, String> currentParamsValuesVec = new HashMap<>();
+    HashMap<String, StructureMatrix<Double>> currentParamsValuesVecComputed = new HashMap<>();
+    public class Instruction {
         private int id;
         private String leftHand;
         private String expression;
@@ -75,7 +68,6 @@ public class ListInstructions {
             this.expression = expression;
         }
     }
-
     private ArrayList<Instruction> assignations;
 
     public ListInstructions() {
@@ -91,16 +83,13 @@ public class ListInstructions {
     }
 
     public void addInstructions(@NotNull String toString) {
-        if(assignations==null)
-            assignations = new ArrayList<>();
 
-
-        if (toString != null && !toString.isEmpty()) {
+        if(toString!=null && !toString.isEmpty()) {
             assignations = new ArrayList<>();
 
             String text = toString;
 
-            String[] splitLines = text.split("\n");
+            String [] splitLines = text.split("\n");
 
             for (int i = 0; i < splitLines.length; i++) {
 
@@ -110,7 +99,7 @@ public class ListInstructions {
 
                 String value = null;
                 String variable = null;
-                if (splitInstructionEquals.length == 1) {
+                if(splitInstructionEquals.length==1) {
                     value = splitInstructionEquals[0].trim();
                 }
                 if (splitInstructionEquals.length == 2) {
@@ -118,9 +107,9 @@ public class ListInstructions {
                     value = splitInstructionEquals[1].trim();
                 }
                 boolean assigned = false;
-                if (splitInstructionEquals.length >= 1) {
+                if(splitInstructionEquals.length>=1) {
 
-                    if ((variable != null ? variable.length() : 0) > 0 && Character.isLetter(variable.toCharArray()[0])) {
+                    if ((variable != null ? variable.length() : 0) >0 && Character.isLetter(variable.toCharArray()[0])) {
                         int j = 0;
                         while (j < variable.length() && (Character.isLetterOrDigit(variable.toCharArray()[j])
                                 || variable.toCharArray()[j] == '_')) {
@@ -135,83 +124,66 @@ public class ListInstructions {
                         assigned = true;
                     }
                 }
-                /*if (!assigned) {
-                    if (splitInstructionEquals.length == 1) {
-                        if (!value.startsWith("#")) {
+                if(!assigned) {
+                    if(splitInstructionEquals.length==1) {
+                        if(!value.startsWith("#")) {
                             assignations.add(new Instruction(i, "", splitInstructionEquals[0]));
                         }
                     }
-                }*/
+                }
             }
         }
     }
-
     public List<String> runInstructions() {
         List<String> returnedCode = new ArrayList<>();
         Instruction[] instructions = new Instruction[assignations.size()];
 
         assignations.toArray(instructions);
 
-        if (currentParamsValues == null)
-            currentParamsValues = new HashMap<>();
-        if (currentParamsValuesVec == null)
-            currentParamsValuesVec = new HashMap<>();
-        if (currentParamsValuesVecComputed == null)
-            currentParamsValuesVecComputed = new HashMap<>();
-        int i = 0;
-        int countInstructions = 0;
-        for (Instruction instruction : instructions) {
-            String key = instruction.getLeftHand();
-            String value = instruction.getExpression();
-
-            if(key!=null)
-                key = key.trim();
-            if(value!=null)
-                value = value.trim();
+        currentParamsValues = new HashMap<>();
+        currentParamsValuesVec = new HashMap<>();
+        currentParamsValuesVecComputed = new HashMap<>();
+        int i=0;
+        for(Instruction instruction : instructions) {
+            String key = (String) instruction.getLeftHand();
+            String value = (String) instruction.getExpression();
 
             StructureMatrix<Double> resultVec = null;
             Double resultDouble = null;
             try {
-                AlgebraicTree tree = new AlgebraicTree(value);
-                tree.setParametersValues(currentParamsValues);
-                tree.setParametersValuesVec(currentParamsValuesVec);
-                tree.setParametersValuesVecComputed(currentParamsValuesVecComputed);
+                if(value!=null) {
+                    AlgebraicTree tree = new AlgebraicTree(value);
+                    tree.setParametersValues(currentParamsValues);
+                    tree.setParametersValuesVec(currentParamsValuesVec);
+                    tree.setParametersValuesVecComputed(currentParamsValuesVecComputed);
 
-                tree.construct();
+                    tree.construct();
 
-                resultVec = tree.eval();
+                    resultVec = tree.eval();
 
-                if (resultVec != null) {
-                    System.out.println("key: " + key + " value: " + value + " computed: " + resultVec);
-                    if (resultVec.getDim() == 1) {
-                        currentParamsValuesVecComputed.put(key, resultVec);
-                        currentParamsValuesVec.put(key, value);
-                    } else if (resultVec.getDim() == 0) {
-                        currentParamsValuesVecComputed.put(key, resultVec);
-                        currentParamsValuesVec.put(key, value);
-                    }
-                } else {
-                    if(getCurrentParamsValuesVecComputed().get(key)!=null)
-                        resultVec = getCurrentParamsValuesVecComputed().get(key);
-                    else
+                    if(resultVec != null) {
+                        if(resultVec.getDim()==1 && key!=null) {
+                            currentParamsValuesVecComputed.put(key, resultVec);
+                        } else if(resultVec.getDim()==0 && key!=null) {
+                            currentParamsValuesVecComputed.put(key, resultVec);
+                        }
+                    } else {
                         throw new AlgebraicFormulaSyntaxException("Result was null");
+                    }
+                    System.err.println("AlgebraicTree result : " + tree);
                 }
-                //System.err.println("AlgebraicTree result : " + tree);
             } catch (AlgebraicFormulaSyntaxException | TreeNodeEvalException |
                      NullPointerException e) {
                 e.printStackTrace();
-                i++;
-                continue;
             }
             String errors1 = "";
-            if (value != null && resultVec != null && (!value.startsWith("#")&& !(key==null||key.startsWith("#"))) && !value.isBlank() && !value.equals("null")) {
-                errors1 += String.format(Locale.getDefault(), "\n#line : (%d)%s ", countInstructions, resultVec.toStringLine());
+            if(value!=null && resultVec!=null && (!value.startsWith("# ") && !value.isBlank() && !value.equals("null")) ) {
+                errors1 += String.format(Locale.getDefault(), "# Result of line : (%d) <<< %s ", i, resultVec.toStringLine());
             }
-            if (value != null && (!value.startsWith("#")&& !(key==null||key.startsWith("#")))) {
-                errors1 += "\n" + (key == null || key.isBlank() ? "" : (key + "=")) + value;
-                countInstructions ++;
+            if(value!=null && (!value.startsWith("# "))) {
+                errors1 += "\n" + (key == null||key.isBlank() ? "" : (key + "=")) + value;
             }
-            if (!(errors1.isBlank() || errors1.equals("null"))) {
+            if(!(errors1.isBlank()||errors1.equals("null"))) {
                 returnedCode.add(errors1);
             }
             i++;
@@ -230,17 +202,5 @@ public class ListInstructions {
 
     public HashMap<String, StructureMatrix<Double>> getCurrentParamsValuesVecComputed() {
         return currentParamsValuesVecComputed;
-    }
-
-    public void setCurrentParamsValues(HashMap<String, Double> currentVars) {
-        this.currentParamsValues = currentVars;
-    }
-
-    public void setCurrentParamsValuesVec(HashMap<String, String> currentVecs) {
-        this.currentParamsValuesVec = currentVecs;
-    }
-
-    public void setCurrentParamsValuesVecComputed(HashMap<String, StructureMatrix<Double>> currentVecsComputed) {
-        this.currentParamsValuesVecComputed = currentVecsComputed;
     }
 }
